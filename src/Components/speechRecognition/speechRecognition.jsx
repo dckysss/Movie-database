96062ce65a3810a5ab2a7ec865feb47ba3fe65c4
Dicky Sanjaya <dickysanjaya003@gmail.com>
2023-05-31 10:37:31 +0700

    speech recognition update

--- src/Components/speechRecognition/speechRecognition.jsx
+++ src/Components/speechRecognition/speechRecognition.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 import './speechRecognition.css'
 import microphone from "../../Assets/microphone.svg"
@@ -6,6 +6,8 @@ import microphone from "../../Assets/microphone.svg"
 const SpeechToText = ({ setSearchQuery }) => {
 
     const { transcript, resetTranscript, browserSupportsSpeechRecognition } = useSpeechRecognition();
+    const timeoutRef = useRef(null);
+    const [isListening, setIsListening] = useState(false);
     
     useEffect(() => {
         const updateSearchQuery = () => {
@@ -15,13 +17,20 @@ const SpeechToText = ({ setSearchQuery }) => {
     }, [transcript, setSearchQuery]);
 
     const startListening = () => {
-      clearTimeout()
-      resetTranscript();
-      SpeechRecognition.startListening({ continuous: true, language: 'en-ID' });
-
-      setTimeout(() => {
+      if(isListening) {
         SpeechRecognition.stopListening();
-      }, 6000);
+        setIsListening(false);
+      } else {
+        resetTranscript();
+        SpeechRecognition.startListening({ continuous: true, language: 'en-ID' });
+        
+        clearTimeout(timeoutRef.current);
+        setIsListening(true);
+        timeoutRef.current = setTimeout(() => {
+          SpeechRecognition.stopListening();
+          setIsListening(false);
+        }, 6000);
+      }
     };
 
     if(!browserSupportsSpeechRecognition) {
@@ -29,8 +38,14 @@ const SpeechToText = ({ setSearchQuery }) => {
     }
 
     return(
-      <div> 
-        <button onClick={startListening}><img src={microphone} alt='speech' /></button>
+      <div className="speech-container"> 
+        <button onClick={startListening}>
+          <img 
+            src={microphone} 
+            alt='speech' 
+            className={isListening ? 'listening' : ''}
+          />
+        </button>
       </div>  
     )
 }
